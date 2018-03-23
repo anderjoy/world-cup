26d817377c1a8e979b0b11910a594879968b5e7c
Anderson Jesus <ajesus@JDCONSULTORES.COM.BR>
2018-03-23 18:58:48 -0300

    versão inicial

---
+++ Models/AccountViewModels/LoginWithRecoveryCodeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace world_cup.Models.AccountViewModels
+{
+    public class LoginWithRecoveryCodeViewModel
+    {
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Recovery Code")]
+            public string RecoveryCode { get; set; }
+    }
+}
